e5e943c96fe1b5bc9ab47c469d16a61aab1531aa
Kingkaene <33867022+Kingkaene@users.noreply.github.com>
2018-04-03 14:29:06 +0200

    Aanmaak BaasEen en bijwerking Aanval

--- src/main/java/nl/han/ica/killthememe/Aanval.java
+++ src/main/java/nl/han/ica/killthememe/Aanval.java
@@ -1,6 +1,7 @@
 package nl.han.ica.killthememe;
 
+import nl.han.ica.OOPDProcessingEngineHAN.Objects.Sprite;
+
 public class Aanval {
 	
-
 }
--- src/main/java/nl/han/ica/killthememe/Vijand.java
+++ src/main/java/nl/han/ica/killthememe/Vijand.java
@@ -1,5 +1,8 @@
 package nl.han.ica.killthememe;
 
+
+import java.awt.image.BufferedImage;
+
 import nl.han.ica.OOPDProcessingEngineHAN.Objects.AnimatedSpriteObject;
 import nl.han.ica.OOPDProcessingEngineHAN.Objects.Sprite;
 import nl.han.ica.OOPDProcessingEngineHAN.Objects.SpriteObject;
@@ -8,13 +11,23 @@ public class Vijand extends SpriteObject{
 	
 	private Level level;
 	private MainGame mainGame;
+	private Sprite projectileSprite;
+
 	
 	
 	public Vijand(MainGame mainGame) {
 		 this(new Sprite("src/main/java/nl/han/ica/killthememe/media/sprite1.png"));
 	     this.mainGame=mainGame;
+	     projectileSprite = new Sprite();
+
+	}
+	public void afschieten() {
+		float direction = getAngleFrom(mainGame.getVijand());
+		Aanval aanval = new Aanval(mainGame, projectileSprite, direction, 10, true, 8);
+		mainGame.addGameObject(Aanval, getX() + getWidth() / 2 - Aanval.WIDTH / 2, getY() + getHeight());
 	}
 	
+ 	
     private Vijand(Sprite sprite) {
         super(sprite);
         setxSpeed(-1);
@@ -28,3 +41,5 @@ public class Vijand extends SpriteObject{
 
     }
 }
+
+
