bb39cbef73cb207635476886b05e9be990b0a509
Kingkaene <33867022+Kingkaene@users.noreply.github.com>
2018-04-03 14:32:14 +0200

    ...

--- src/main/java/nl/han/ica/killthememe/Aanval.java
+++ src/main/java/nl/han/ica/killthememe/Aanval.java
@@ -3,5 +3,11 @@ package nl.han.ica.killthememe;
 import nl.han.ica.OOPDProcessingEngineHAN.Objects.Sprite;
 
 public class Aanval {
+	public static int WIDTH = 11;
+	
+	
+	void aanval() {
+		
+	}
 	
 }
--- src/main/java/nl/han/ica/killthememe/BaasEen.java
+++ src/main/java/nl/han/ica/killthememe/BaasEen.java
@@ -1,5 +1,22 @@
 package nl.han.ica.killthememe;
 
-public class BaasEen {
+import nl.han.ica.OOPDProcessingEngineHAN.Objects.Sprite;
 
+public class BaasEen {
+	private Sprite projectileSprite;
+	private MainGame mainGame;
+	
+	
+	public BaasEen(MainGame mainGame) {
+		 this(new Sprite("src/main/java/nl/han/ica/killthememe/media/sprite1.png"));
+	     this.mainGame=mainGame;
+	     projectileSprite = new Sprite("src/main/java/nl/han/ica/killthememe/media/sprite1.png");
+
+	}
+	public void afschieten() {
+		float direction = getAngleFrom(mainGame.getVijand());
+		Aanval aanval = new Aanval(mainGame, projectileSprite, direction, 10, true, 8);
+		mainGame.addGameObject(aanval, getX() + getWidth() / 2 - Aanval.WIDTH / 2, getY() + getHeight());
+	}
+	
 }
