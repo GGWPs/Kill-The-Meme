088dafd6622b833399656133d368b7bfce1d1b0a
IMila <IMila@DESKTOP-PORA2F2>
2018-10-26 14:18:38 +0200

    test

--- src/main/java/nl/han/ica/killthememe/MainGame.java
+++ src/main/java/nl/han/ica/killthememe/MainGame.java
@@ -37,7 +37,13 @@ public class MainGame extends GameEngine implements IAlarmListener {
 	private int worldHeight;
 
 	public static void main(String[] args) {
-		PApplet.main(new String[] { "nl.han.ica.killthememe.MainGame" });
+		//PApplet.main(new String[] {"nl.han.ica.killthememe.MainGame"});
+		
+		String[] processingArgs = {"nl.han.ica.killthememe.MainGame"};
+		MainGame mySketch = new MainGame();
+		
+		PApplet.runSketch(processingArgs, mySketch);
+		
 	}
 
 	/**
